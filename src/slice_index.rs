5b493a12cf7e5db7c600b398c0f2618c11635731
Christian Poveda <git@pvdrz.com>
2021-11-24 09:56:15 -0500

    implement `SliceIndex` for "non const" indices

--- src/slice_index.rs
+++ src/slice_index.rs
@@ -1,4 +1,6 @@
-use core::ops::{Index, IndexMut};
+use core::ops::{
+    Index, IndexMut, Range, RangeFrom, RangeFull, RangeInclusive, RangeTo, RangeToInclusive,
+};
 
 use crate::range::{ConstRange, ConstRangeInclusive, ConstRangeTo, ConstRangeToInclusive};
 use crate::usize::ConstUsize;
@@ -16,11 +18,19 @@ mod sealed {
     }
     impl<const MAX: usize> Sealed for ConstRangeTo<MAX> {}
     impl<const MAX: usize, const LEN: usize> Sealed for ConstRangeToInclusive<MAX, LEN> {}
+
+    impl Sealed for usize {}
+    impl Sealed for Range<usize> {}
+    impl Sealed for RangeFrom<usize> {}
+    impl Sealed for RangeFull {}
+    impl Sealed for RangeInclusive<usize> {}
+    impl Sealed for RangeTo<usize> {}
+    impl Sealed for RangeToInclusive<usize> {}
 }
 
 /// This trait is a line by line copy of [`SliceIndex`](core::slice::SliceIndex).
 pub unsafe trait SliceIndex<T: ?Sized>: sealed::Sealed {
-    type Output;
+    type Output: ?Sized;
 
     fn get(self, slice: &T) -> Option<&Self::Output>;
     fn get_mut(self, slice: &mut T) -> Option<&mut Self::Output>;
@@ -195,5 +205,233 @@ unsafe impl<T, const MAX: usize, const LEN: usize> SliceIndex<[T]>
 
     fn index_mut(self, slice: &mut [T]) -> &mut Self::Output {
         slice.index_mut(self.range()).try_into().unwrap()
+    }
+}
+
+unsafe impl<T> SliceIndex<[T]> for usize {
+    type Output = T;
+
+    #[inline]
+    fn get(self, slice: &[T]) -> Option<&T> {
+        slice.get(self)
+    }
+
+    #[inline]
+    fn get_mut(self, slice: &mut [T]) -> Option<&mut T> {
+        slice.get_mut(self)
+    }
+
+    #[inline]
+    unsafe fn get_unchecked(self, slice: *const [T]) -> *const T {
+        (slice as *const T).add(self)
+    }
+
+    #[inline]
+    unsafe fn get_unchecked_mut(self, slice: *mut [T]) -> *mut T {
+        (slice as *mut T).add(self)
+    }
+
+    #[inline]
+    fn index(self, slice: &[T]) -> &T {
+        slice.index(self)
+    }
+
+    #[inline]
+    fn index_mut(self, slice: &mut [T]) -> &mut T {
+        slice.index_mut(self)
+    }
+}
+
+unsafe impl<T> SliceIndex<[T]> for Range<usize> {
+    type Output = [T];
+
+    #[inline]
+    fn get(self, slice: &[T]) -> Option<&[T]> {
+        slice.get(self)
+    }
+
+    #[inline]
+    fn get_mut(self, slice: &mut [T]) -> Option<&mut [T]> {
+        slice.get_mut(self)
+    }
+
+    #[inline]
+    unsafe fn get_unchecked(self, slice: *const [T]) -> *const [T] {
+        unsafe {
+            core::ptr::slice_from_raw_parts(
+                (slice as *const T).add(self.start),
+                self.end - self.start,
+            )
+        }
+    }
+
+    #[inline]
+    unsafe fn get_unchecked_mut(self, slice: *mut [T]) -> *mut [T] {
+        unsafe {
+            core::ptr::slice_from_raw_parts_mut(
+                (slice as *mut T).add(self.start),
+                self.end - self.start,
+            )
+        }
+    }
+
+    #[inline]
+    fn index(self, slice: &[T]) -> &[T] {
+        slice.index(self)
+    }
+
+    #[inline]
+    fn index_mut(self, slice: &mut [T]) -> &mut [T] {
+        slice.index_mut(self)
+    }
+}
+
+unsafe impl<T> SliceIndex<[T]> for RangeFrom<usize> {
+    type Output = [T];
+
+    #[inline]
+    fn get(self, slice: &[T]) -> Option<&[T]> {
+        slice.get(self)
+    }
+
+    #[inline]
+    fn get_mut(self, slice: &mut [T]) -> Option<&mut [T]> {
+        slice.get_mut(self)
+    }
+
+    #[inline]
+    unsafe fn get_unchecked(self, slice: *const [T]) -> *const [T] {
+        unsafe { (self.start..(*slice).len()).get_unchecked(slice) }
+    }
+
+    #[inline]
+    unsafe fn get_unchecked_mut(self, slice: *mut [T]) -> *mut [T] {
+        unsafe { (self.start..(*slice).len()).get_unchecked_mut(slice) }
+    }
+
+    #[inline]
+    fn index(self, slice: &[T]) -> &[T] {
+        slice.index(self)
+    }
+
+    #[inline]
+    fn index_mut(self, slice: &mut [T]) -> &mut [T] {
+        slice.index_mut(self)
+    }
+}
+
+unsafe impl<T> SliceIndex<[T]> for RangeFull {
+    type Output = [T];
+
+    #[inline]
+    fn get(self, slice: &[T]) -> Option<&[T]> {
+        slice.get(self)
+    }
+
+    #[inline]
+    fn get_mut(self, slice: &mut [T]) -> Option<&mut [T]> {
+        slice.get_mut(self)
+    }
+
+    #[inline]
+    unsafe fn get_unchecked(self, slice: *const [T]) -> *const [T] {
+        slice
+    }
+
+    #[inline]
+    unsafe fn get_unchecked_mut(self, slice: *mut [T]) -> *mut [T] {
+        slice
+    }
+
+    #[inline]
+    fn index(self, slice: &[T]) -> &[T] {
+        slice.index(self)
+    }
+
+    #[inline]
+    fn index_mut(self, slice: &mut [T]) -> &mut [T] {
+        slice.index_mut(self)
+    }
+}
+
+fn into_slice_range(range: RangeInclusive<usize>) -> Range<usize> {
+    let exclusive_end = *range.end() + 1;
+    let start = if range.is_empty() {
+        exclusive_end
+    } else {
+        *range.start()
+    };
+    start..exclusive_end
+}
+
+unsafe impl<T> SliceIndex<[T]> for RangeInclusive<usize> {
+    type Output = [T];
+
+    #[inline]
+    fn get(self, slice: &[T]) -> Option<&[T]> {
+        slice.get(self)
+    }
+
+    #[inline]
+    fn get_mut(self, slice: &mut [T]) -> Option<&mut [T]> {
+        slice.get_mut(self)
+    }
+
+    #[inline]
+    unsafe fn get_unchecked(self, slice: *const [T]) -> *const [T] {
+        // SAFETY: the caller has to uphold the safety contract for `get_unchecked`.
+        unsafe { into_slice_range(self).get_unchecked(slice) }
+    }
+
+    #[inline]
+    unsafe fn get_unchecked_mut(self, slice: *mut [T]) -> *mut [T] {
+        // SAFETY: the caller has to uphold the safety contract for `get_unchecked_mut`.
+        unsafe { into_slice_range(self).get_unchecked_mut(slice) }
+    }
+
+    #[inline]
+    fn index(self, slice: &[T]) -> &[T] {
+        slice.index(self)
+    }
+
+    #[inline]
+    fn index_mut(self, slice: &mut [T]) -> &mut [T] {
+        slice.index_mut(self)
+    }
+}
+
+unsafe impl<T> SliceIndex<[T]> for RangeTo<usize> {
+    type Output = [T];
+
+    #[inline]
+    fn get(self, slice: &[T]) -> Option<&[T]> {
+        slice.get(self)
+    }
+
+    #[inline]
+    fn get_mut(self, slice: &mut [T]) -> Option<&mut [T]> {
+        slice.get_mut(self)
+    }
+
+    #[inline]
+    unsafe fn get_unchecked(self, slice: *const [T]) -> *const [T] {
+        // SAFETY: the caller has to uphold the safety contract for `get_unchecked`.
+        unsafe { (0..self.end).get_unchecked(slice) }
+    }
+
+    #[inline]
+    unsafe fn get_unchecked_mut(self, slice: *mut [T]) -> *mut [T] {
+        // SAFETY: the caller has to uphold the safety contract for `get_unchecked_mut`.
+        unsafe { (0..self.end).get_unchecked_mut(slice) }
+    }
+
+    #[inline]
+    fn index(self, slice: &[T]) -> &[T] {
+        slice.index(self)
+    }
+
+    #[inline]
+    fn index_mut(self, slice: &mut [T]) -> &mut [T] {
+        slice.index_mut(self)
     }
 }
